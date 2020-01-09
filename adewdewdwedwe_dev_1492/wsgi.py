769754c4add0a274e97b37fc701b77f1bc752270
Crowdbotics <team@crowdbotics.com>
2020-01-09 15:25:18 +0000

    Update models

--- adewdewdwedwe_dev_1492/wsgi.py
+++ adewdewdwedwe_dev_1492/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adewdewdwedwe_dev_1492.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adewdewdwedwe_dev_1492.settings")
 
 application = get_wsgi_application()
--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,18 +7,18 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150,)
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
 
 
 class HomePage(models.Model):
@@ -25,8 +26,15 @@ class HomePage(models.Model):
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
+
+
+class R1(models.Model):
+    "Generated Model"
+    f1 = models.DateField()
+    f2 = models.DateTimeField(auto_now=True,)
+    f3 = models.TimeField(auto_now_add=True,)
